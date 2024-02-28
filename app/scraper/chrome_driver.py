e81f12975a7deefb763c62da78120f8701b43134
Michael Maluf <malufmike@gmail.com>
2024-02-27 22:49:02 -0800

    Enhances scraper to handle all responsibilities for getting match URLs and returning match page sources

--- app/scraper/chrome_driver.py
+++ app/scraper/chrome_driver.py
@@ -1,4 +1,10 @@
+from typing import List, Dict, Tuple, Optional
+import time
+import os
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class ChromeDriver:
     def __init__(self):
@@ -7,7 +13,7 @@ class ChromeDriver:
 
     def get_headers(self):
         headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+            "User-Agent": os.getenv('SCRAPER_USER_AGENT'),
         }
         return headers
 
@@ -23,10 +29,27 @@ class ChromeDriver:
             chrome_options.add_argument(f"--{key}={value}")
         return webdriver.Chrome(options=chrome_options)
 
-    def get_webpage(self, url):
-        self.driver.get(url)
-        webpage = self.driver.page_source
-        self.driver.close()
-        self._driver = None
-        return webpage
+    def fetch_match_urls_from_match_dates(self, match_date_urls: List[str]) -> List[str]:
+        # matches are grouped by their dates, this method scrapes the match urls for each date
 
+        match_urls = []
+        for match_date_url in match_date_urls:
+            self.driver.get(match_date_url)
+            time.sleep(3)
+            match_divs = self.driver.find_elements(By.CLASS_NAME, 'mantine-vdx6qn')
+
+            for match_div in match_divs:
+                anchor_tag = match_div.find_element(By.TAG_NAME, 'a')
+                match_path = anchor_tag.get_attribute('href')
+                match_urls.append(match_path)
+
+        return match_urls
+
+    def fetch_match_page_source(self, match_url: str):
+        self.driver.get(match_url)
+        time.sleep(3)
+        return self.driver.page_source
+
+    def exit_driver(self):
+        self.driver.close()
+        _driver = None
