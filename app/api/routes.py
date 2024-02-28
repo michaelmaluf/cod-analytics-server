c2ddf17dddaee97e02982f6241f13f9d6b537de2
Michael Maluf <malufmike@gmail.com>
2024-02-27 22:57:39 -0800

    Adds scraper blueprint and endpoint for activating the scraper as well as creating the database entries from the scraper results

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,12 +1,17 @@
 import json
 
 from flask import Blueprint, Response
-from app.scraper.data_scraper import get_most_recent_match_date
+
+from app.scraper.data_scraper import fetch_match_and_player_data
+from app.services import MatchDataCoordinatorService
+from app import db
 
 
 scraper_bp = Blueprint('scraper', __name__, url_prefix='/scraper')
 
 @scraper_bp.route('/run', methods=['GET'])
 def scrape():
-    get_most_recent_match_date()
+    scraped_data = fetch_match_and_player_data()
+    coordinator_service = MatchDataCoordinatorService(db.session)
+    coordinator_service.populate_all_data(scraped_data)
     return Response('Success', 200)
