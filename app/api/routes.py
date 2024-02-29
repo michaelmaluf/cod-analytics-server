072c73e41ebea1ffbf314e8c678c0d1b779511ee
Michael Maluf <malufmike@gmail.com>
2024-02-28 23:08:22 -0800

    Modifications to configurations in the scraper endpoint and const.py files

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,7 @@ import json
 from flask import Blueprint, Response
 
 from app.scraper.data_scraper import fetch_match_and_player_data
-from app.services import MatchDataCoordinatorService
+from app.services import CompetitiveDataSyncService
 from app import db
 
 
@@ -12,6 +12,6 @@ scraper_bp = Blueprint('scraper', __name__, url_prefix='/scraper')
 @scraper_bp.route('/run', methods=['GET'])
 def scrape():
     scraped_data = fetch_match_and_player_data()
-    coordinator_service = MatchDataCoordinatorService(db.session)
+    coordinator_service = CompetitiveDataSyncService(db.session)
     coordinator_service.populate_all_data(scraped_data)
     return Response('Success', 200)
