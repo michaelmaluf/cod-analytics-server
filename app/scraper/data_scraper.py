8cc194d62df11049403d355eb5b0e366c585a5bc
Michael Maluf <malufmike@gmail.com>
2024-02-28 23:01:59 -0800

    Enhances dating scraping utility functions to handle all functionality related to parsing and scraping data

--- app/scraper/data_scraper.py
+++ app/scraper/data_scraper.py
@@ -1,6 +1,6 @@
 import os
 from datetime import date, datetime
-from calendar import monthcalendar
+from calendar import Calendar
 
 from .chrome_driver import ChromeDriver
 from app import db
@@ -17,16 +17,16 @@ def get_most_recent_match_date():
     most_recent_match_date = db.session.query(db.func.max(Match.date)).scalar()
 
     if not most_recent_match_date:
-        most_recent_match_date = const.CDL_FIRST_MATCH_DATE
+        most_recent_match_date = datetime.strptime(const.CDL_FIRST_MATCH_DATE, '%Y-%m-%d').date()
 
     return most_recent_match_date
 
 
 def get_match_dates_until_present():
     last_match_date = get_most_recent_match_date()
-    last_match_date_month = int(last_match_date.split('-')[1])
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    current_month = int(current_date.split('-')[1])
+    last_match_date_month = int(last_match_date.strftime('%Y-%m-%d').split('-')[1])
+    current_date = datetime.now().date()
+    current_month = int(current_date.strftime('%Y-%m-%d').split('-')[1])
     year = const.CDL_YEAR
 
     if last_match_date_month <= current_month:
@@ -37,15 +37,16 @@ def get_match_dates_until_present():
         year_month_pairs = [*range1, *range2]
 
     days_of_week = [3, 4, 5, 6]
+    match_dates = []
+    cal = Calendar()
 
-    match_dates = [
-        date(year, month, day)
-        for year, month in year_month_pairs
-        for day_of_week in days_of_week
-        for week in monthcalendar(year, month)
-        for day in [week[day_of_week]]
-        if day != 0
-    ]
+    for year, month in year_month_pairs:
+        for day in cal.itermonthdays(year, month):
+            if day == 0:
+                continue
+            potential_date = date(year, month, day)
+            if potential_date.weekday() in days_of_week and last_match_date < potential_date <= current_date:
+                match_dates.append(potential_date)
 
     return match_dates
 
@@ -55,9 +56,13 @@ def get_urls_by_match_dates():
     return [const.BREAKING_POINT_DATE_URL + match_date.strftime('%Y-%m-%d') for match_date in match_dates_to_fetch]
 
 
-def parse_match_stage(stage_div):
-    stage_element = stage_div.find('a').text.split(' ')
-    return ' '.join(stage_element[1:3])
+def parse_match_metadata(match_metadata_div):
+    metadata_element = match_metadata_div.find('a').text.split(' ')
+    return {
+        'stage': ' '.join(metadata_element[1:3]),
+        'lan': True if metadata_element[-1] != 'Qualifier' else False,
+    }
+
 
 
 def parse_match_overview(match_overview_div):
@@ -66,9 +71,9 @@ def parse_match_overview(match_overview_div):
     team_names = match_overview_div.find_all('div', class_='mantine-14yjo12')
     match_results = match_overview_div.find('div', class_='mantine-1uguyhf').find_all('div')
 
-    match_overview['team_one'] = team_names[0].find('a').text
+    match_overview['team_one_name'] = team_names[0].find('a').text
     match_overview['team_one_maps_won'] = int(match_results[0].text)
-    match_overview['team_two'] = team_names[1].find('a').text
+    match_overview['team_two_name'] = team_names[1].find('a').text
     match_overview['team_two_maps_won'] = int(match_results[2].text)
 
     return match_overview
@@ -106,16 +111,19 @@ def parse_player_data_for_map(player_data_div):
 def parse_match_maps(match_maps_overview_div, player_data_by_match_map_divs):
     match_maps = []
     match_maps_overview_divs = match_maps_overview_div.find_all('div', class_='mantine-155beqj')
+    map_number = 1
 
     for overview_div, player_data_div in zip(match_maps_overview_divs, player_data_by_match_map_divs):
         match_map = {}
         map_results = overview_div.find_all('div', class_='mantine-x468zj')
         match_map['map'] = overview_div.find('div', class_='mantine-6cg8ua').text
         match_map['game_mode'] = overview_div.find('div', class_='mantine-1ey6z4x').text
+        match_map['map_number'] = map_number
         match_map['team_one_score'] = int(map_results[0].text)
         match_map['team_two_score'] = int(map_results[2].text)
         match_map.update(parse_player_data_for_map(player_data_div))
         match_maps.append(match_map)
+        map_number += 1
 
     return match_maps
 
@@ -124,15 +132,15 @@ def parse_match_data(match_url):
     match_page_source = chrome_driver.fetch_match_page_source(match_url)
     soup = BeautifulSoup(match_page_source, 'html.parser')
 
-    match_date = soup.find('div', class_='mantine-7c77qh').text
-    stage_div = soup.find('div', class_='mantine-l3a1ir')
+    match_date = soup.find('div', class_='mantine-7c77qh').text.split(',')[0]
+    match_metadata_div = soup.find('div', class_='mantine-l3a1ir')
     match_overview_div = soup.find('div', class_='mantine-1a5yjft')
     match_maps_overview_div = soup.find('div', class_='mantine-g92whd')
     player_data_by_match_map_divs = soup.find_all('div', class_='mantine-v1hkmm')
 
     match_data = {
-        'match_date': match_date,
-        'stage': parse_match_stage(stage_div),
+        'date': match_date,
+        **parse_match_metadata(match_metadata_div),
         **parse_match_overview(match_overview_div),
         'match_maps': parse_match_maps(match_maps_overview_div, player_data_by_match_map_divs)
     }
@@ -142,7 +150,7 @@ def parse_match_data(match_url):
 
 def fetch_match_and_player_data():
     match_date_urls = get_urls_by_match_dates()
-    match_urls = chrome_driver.fetch_match_urls_from_match_dates(match_date_urls[6:10])
+    match_urls = chrome_driver.fetch_match_urls_from_match_dates(match_date_urls)
     match_and_player_data = []
 
     for match_url in match_urls:
