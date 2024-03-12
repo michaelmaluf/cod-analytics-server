6ea54729e4f6a3f16b482bfd44b9062defb5be2e
Michael Maluf <malufmike@gmail.com>
2024-03-11 20:54:48 -0700

    Adds model_service class which will be responsible for managing the models and performing predictions via API requests

---
+++ app/services/model_service.py
@@ -0,0 +1,37 @@
+import joblib
+from io import BytesIO
+
+from sqlalchemy import desc
+
+from database.models import MLModel, GameMode
+from app.enums import GameModeType
+
+
+# use pickle to serialize and deserailize models.
+
+
+class ModelService:
+    def __init__(self, session):
+        self.session = session
+        self.HardpointModel = None
+        self.SearchAndDestroyModel = None
+        self.ControlModel = None
+        self.fetch_models_from_database()
+
+
+    def fetch_models_from_database(self):
+        for game_mode in GameModeType:
+            game_mode_model = self.session.query(GameMode, MLModel) \
+                .join(MLModel)\
+                .filter(GameMode.name == game_mode) \
+                .order_by(desc(MLModel.creation_date)) \
+                .first()[1]
+
+            model_buffer = BytesIO(game_mode_model.model_data)
+
+            if game_mode == GameModeType.HARDPOINT:
+                self.HardpointModel = joblib.load(model_buffer)
+            elif game_mode == GameModeType.SEARCH_AND_DESTROY:
+                self.SearchAndDestroyModel = joblib.load(model_buffer)
+            else:
+                self.ControlModel = joblib.load(model_buffer)
