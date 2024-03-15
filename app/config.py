5512790fc2c3c9ddbe22dd0e7f59e2f0bf29a44e
Michael Maluf <malufmike@gmail.com>
2024-03-14 21:00:29 -0700

    Additions to config and const files

--- app/config.py
+++ app/config.py
@@ -3,12 +3,21 @@ import os
 class Config:
     DEBUG = False
     TESTING = False
+    API_TITLE = 'COD API'
+    API_VERSION = 'v1'
+    OPENAPI_VERSION = '3.0.2'
+
 
 class DevConfig(Config):
     FLASK_ENV = 'development'
     SECRET_KEY = 'dev'
     SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL')
     DEBUG = True
+    OPENAPI_JSON_PATH = 'openapi.json'
+    OPENAPI_URL_PREFIX = '/'
+    OPENAPI_SWAGGER_UI_PATH = '/swagger-ui'
+    OPENAPI_SWAGGER_UI_URL = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
