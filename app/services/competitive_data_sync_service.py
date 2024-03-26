605ea39bb93f1a08cc3af2b8d225804dab096796
Michael Maluf <malufmike@gmail.com>
2024-03-26 09:32:23 -0700

    Adds functionality for updating a player_team_status after the player is no longer on the team

--- app/services/competitive_data_sync_service.py
+++ app/services/competitive_data_sync_service.py
@@ -19,21 +19,38 @@ class CompetitiveDataSyncService:
             self.session.add(match)
 
             match_maps_data = data['match_maps']
+            team_one_player_ids = set()
+            team_two_player_ids = set()
 
             for match_map_data in match_maps_data:
-                map_game_mode_pair = self.MapGameModeService.get_or_create_map_mode_pair(match_map_data.get('map'), match_map_data.get('game_mode'))
+                map_game_mode_pair = self.MapGameModeService.get_or_create_map_mode_pair(match_map_data.get('map'),
+                                                                                         match_map_data.get(
+                                                                                             'game_mode'))
                 match_map = self.MatchService.create_match_map(match, map_game_mode_pair, match_map_data)
                 self.session.add(match_map)
 
                 for raw_player_data in match_map_data['team_one_player_data']:
                     player = self.RosterService.get_or_create_player(team_one, raw_player_data['name'])
-                    match_map.player_data.append(self.MatchService.create_player_data(player, raw_player_data, map_game_mode_pair))
+                    team_one_player_ids.add(player.id)
+                    match_map.player_data.append(
+                        self.MatchService.create_player_data(player, raw_player_data, map_game_mode_pair))
 
                 for raw_player_data in match_map_data['team_two_player_data']:
                     player = self.RosterService.get_or_create_player(team_two, raw_player_data['name'])
-                    match_map.player_data.append(self.MatchService.create_player_data(player, raw_player_data, map_game_mode_pair))
+                    team_two_player_ids.add(player.id)
+                    match_map.player_data.append(
+                        self.MatchService.create_player_data(player, raw_player_data, map_game_mode_pair))
 
                 match.match_maps.append(match_map)
+
+            # updates team statuses for players who are no longer on the team
+            for player_team_status in team_one.player_statuses:
+                if player_team_status.active and player_team_status.player_id not in team_one_player_ids:
+                    player_team_status.active = False
+
+            for player_team_status in team_two.player_statuses:
+                if player_team_status.active and player_team_status.player_id not in team_two_player_ids:
+                    player_team_status.active = False
 
             self.session.commit()
             self.session.close()
