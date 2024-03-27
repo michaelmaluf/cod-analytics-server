f56d288cd49027b2af878e0bfadb9f34cd9a29e2
Michael Maluf <malufmike@gmail.com>
2024-03-27 00:01:54 -0700

    Adds try catch blocks for fetching ML models from db

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -17,18 +17,21 @@ class ModelService:
 
 
     def fetch_models_from_database(self):
-        for game_mode in GameModeType:
-            game_mode_model = self.session.query(GameMode, MLModel) \
-                .join(MLModel)\
-                .filter(GameMode.name == game_mode) \
-                .order_by(desc(MLModel.creation_date)) \
-                .first()[1]
+        try:
+            for game_mode in GameModeType:
+                game_mode_model = self.session.query(GameMode, MLModel) \
+                    .join(MLModel)\
+                    .filter(GameMode.name == game_mode) \
+                    .order_by(desc(MLModel.creation_date)) \
+                    .first()[1]
 
-            model_buffer = BytesIO(game_mode_model.model_data)
+                model_buffer = BytesIO(game_mode_model.model_data)
 
-            if game_mode == GameModeType.HARDPOINT:
-                self.HardpointModel = joblib.load(model_buffer)
-            elif game_mode == GameModeType.SEARCH_AND_DESTROY:
-                self.SearchAndDestroyModel = joblib.load(model_buffer)
-            else:
-                self.ControlModel = joblib.load(model_buffer)
+                if game_mode == GameModeType.HARDPOINT:
+                    self.HardpointModel = joblib.load(model_buffer)
+                elif game_mode == GameModeType.SEARCH_AND_DESTROY:
+                    self.SearchAndDestroyModel = joblib.load(model_buffer)
+                else:
+                    self.ControlModel = joblib.load(model_buffer)
+        except TypeError:
+            print('ML Models table is empty, cannot load models.')
